c5593a5063c3897c5cc8865288f8152e085f77c7
DsDAL <shark02435@sharkcoders.pt>
2024-10-11 20:07:44 +0100

    Add files via upload

---
+++ metro.py
@@ -0,0 +1,10 @@
+m = int(input("Quantos metros?"))
+
+dam = (m/10)
+hm = (dam/10)
+km = (hm/10)
+dm = (m*10)
+cm = (dm*10)
+mm = (cm*10)
+
+print(km,"km =",hm,"hm =",dam,"dam =",m,"m =", dm, "dm =",cm, "cm =",mm,"mm")
